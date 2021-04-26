89db3e75c196d92888ce562b6be7a9d992a75643
fairuzhumaira <hhfairuz@gmail.com>
2021-04-26 15:55:49 +0600

    basic

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
+
+//ReactDOM.render(
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>,
+  //document.getElementById('root')
+//);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
+
+function Basic() {
+  return( 
+  <div>
+    <h1>Hello world</h1>
+    <p> this is done by ahha</p>
+    <ul>q</ul>
+    <ul>ewew</ul>
+    </div>
+
+
+  )
+}
+
+ReactDOM.render(<Basic />, document.getElementById('root')
+
+)
